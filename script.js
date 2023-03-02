05172c422949c56f296bfe78486a671100691662
ketanjindal12 <93584081+ketanjindal12@users.noreply.github.com>
2023-03-02 11:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+var len =str.length;
+var msg ="It is a pallidrome"
+	for(var i=0; i<n/2; i++)
+		{
+			if(str[i] !=str[len-1-i])
+			{
+				msg= It is not palidrone
+			}
+		}
+		console.log(${msg});
 }
 module.exports = palindrome
